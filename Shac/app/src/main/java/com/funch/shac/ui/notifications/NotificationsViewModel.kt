29dc6333ef7375cab9fb019d11caadf9c0b4d331
kieanupark <kieanupark@gmail.com>
2023-08-24 11:14:10 +0900

    feat : 프로젝트 생성

---
+++ Shac/app/src/main/java/com/funch/shac/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.funch.shac.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
